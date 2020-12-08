308e248a0de6a966f8cb1de3735b017a9ef0ac99
Sharvari Divekar <sharvaridivekar09@gmail.com>
2020-12-07 23:06:07 -0500

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Sharvari on 2020-12-07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
